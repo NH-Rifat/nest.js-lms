9ad73a666a4b30ae7a3783b368053a9114ef111b
Hasan Rifat <hasanrifat@Hasans-Mac-mini.local>
2025-10-20 13:21:08 +0600

    added roles decorator

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -6,16 +6,22 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/user/user.types';
 
 @Controller('courses')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
+  @Roles(Role.ADMIN)
   create(@Body() createCourseDto: CreateCourseDto) {
     return this.courseService.create(createCourseDto);
   }
